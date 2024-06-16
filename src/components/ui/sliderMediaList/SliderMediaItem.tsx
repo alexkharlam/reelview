29e6016dea2a70b8835d58b9725516f31d7c7c4c
Alex Kharlamov <aakharlam@gmail.com>
2024-06-16 20:07:37 +0400

    style: set strict slider image height

--- src/components/ui/sliderMediaList/SliderMediaItem.tsx
+++ src/components/ui/sliderMediaList/SliderMediaItem.tsx
@@ -12,7 +12,7 @@ type Props = {
 const SliderMediaItem = ({ posterImgSrc, voteAverage, title, linkPath, year }: Props) => {
   return (
     <Link to={linkPath} className="mx-1 flex w-5.5 flex-col bg-gray-1200 sm:w-6 lg:w-6.5">
-      <img className="w-full" src={posterImgSrc} alt={title} />
+      <img className="h-6.5 w-full sm:h-7" src={posterImgSrc} alt={title} />
       <div className="line-clamp-2 h-3.5 p-1 text-xs font-medium text-white">{title}</div>
       <div className="flex items-center justify-between p-1.5">
         <RatingValue className="text-sm" value={voteAverage} />
